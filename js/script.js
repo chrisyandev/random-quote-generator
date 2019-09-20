5b8083ee5d4e81339c3eb332c58efb5b9e3ec7fd
chrisyandev <czyanhome@hotmail.com>
2019-09-19 21:26:10 -0700

    Changed quotation marks according to Google's styling guide

--- js/script.js
+++ js/script.js
@@ -6,26 +6,26 @@ project 1 - A Random Quote Generator
 // List of quotes
 var quotes = [
 	{
-		quote: "It's the possibility of having a dream come true that makes life interesting.",
-		source: "Paulo Coelho",
-		citation: "The Alchemist",
-		year: "1988"
+		quote: `It's the possibility of having a dream come true that makes life interesting.`,
+		source: 'Paulo Coelho',
+		citation: 'The Alchemist',
+		year: '1988'
 	},
 	{
-		quote: "To get over a mountain, you have to climb it.",
-		source: "Jonathan Tesch"
+		quote: 'To get over a mountain, you have to climb it.',
+		source: 'Jonathan Tesch'
 	},
 	{
-		quote: "Unbeing dead isn't being alive.",
-		source: "E.E. Cummings"
+		quote: `Unbeing dead isn't being alive.`,
+		source: 'E.E. Cummings'
 	},
 	{
-		quote: "Count your age by friends, not years. Count your life by smiles, not tears.",
-		source: "John Lennon"
+		quote: 'Count your age by friends, not years. Count your life by smiles, not tears.',
+		source: 'John Lennon'
 	},
 	{
-		quote: "Success is never final.",
-		source: "Adamson and Steckel"
+		quote: 'Success is never final.',
+		source: 'Adamson and Steckel'
 	}
 ];
 
@@ -38,20 +38,20 @@ function getRandomQuote() {
 // Writes the properties of the random quote object if they exist
 function printQuote() {
   var randomQuote = getRandomQuote(); 
-  var html = "";
+  var html = '';
   
-  html += "<p class='quote'>" + randomQuote.quote + "</p>";
-  html += "<p class='source'>" + randomQuote.source;
+  html += '<p class="quote">' + randomQuote.quote + '</p>';
+  html += '<p class="source">' + randomQuote.source;
   if (randomQuote.citation) {
-    html += "<span class='citation'>" + randomQuote.citation + "</span>";
+    html += '<span class="citation">' + randomQuote.citation + '</span>';
   }
   if (randomQuote.year) {
-    html += "<span class='year'>" + randomQuote.year + "</span>";
+    html += '<span class="year">' + randomQuote.year + '</span>';
   }
-  html += "</p>";
+  html += '</p>';
 
-  document.getElementById("quote-box").innerHTML = html;
+  document.getElementById('quote-box').innerHTML = html;
 }
 
 // Button press invokes the `printQuote` function
-document.getElementById("loadQuote").addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener('click', printQuote, false);
